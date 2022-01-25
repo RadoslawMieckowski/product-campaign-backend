3d603aff5a5f401cc908ae6df39e953d0916e3ab
Radosław Mieckowski <rmieckowski@wp.pl>
2022-01-25 12:05:33 +0100

    Initial commit

---
+++ src/main/java/com/example/productcampaignbackend/ProductCampaignBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.productcampaignbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCampaignBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCampaignBackendApplication.class, args);
+    }
+
+}
