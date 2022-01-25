832d0d3c8cb28ff0bfc76156f4a396ae0dd41546
Radosław Mieckowski <rmieckowski@wp.pl>
2022-01-25 19:56:30 +0100

    add getCampaignById method in controller

--- src/main/java/com/example/productcampaignbackend/controller/CampaignController.java
+++ src/main/java/com/example/productcampaignbackend/controller/CampaignController.java
@@ -1,8 +1,10 @@
 package com.example.productcampaignbackend.controller;
 
+import com.example.productcampaignbackend.exception.ResourceNotFoundException;
 import com.example.productcampaignbackend.model.Campaign;
 import com.example.productcampaignbackend.repository.CampaignRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,8 +21,22 @@ public class CampaignController {
     public List<Campaign> getAllCampaignes() {
         return campaignRepository.findAll();
     }
+
+    @GetMapping("/campaignes/{id}")
+    public ResponseEntity<Campaign> getCampaignById(@PathVariable long id) {
+        Campaign campaign = null;
+        try {
+            campaign = campaignRepository.findById(id)
+                    .orElseThrow(()->new ResourceNotFoundException("Campaign of Id number "+id+" doesn't exist."));
+        } catch (ResourceNotFoundException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok(campaign);
+    }
+
     @PostMapping("/campaignes")
     public Campaign createCampaign(@RequestBody Campaign campaign) {
         return campaignRepository.save(campaign);
     }
+
 }
