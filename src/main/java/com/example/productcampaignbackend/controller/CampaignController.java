b47603421d486efa110ad0bbed2a5075ef028bc7
Radosław Mieckowski <rmieckowski@wp.pl>
2022-01-25 18:06:11 +0100

    add createCampaign method in controller

--- src/main/java/com/example/productcampaignbackend/controller/CampaignController.java
+++ src/main/java/com/example/productcampaignbackend/controller/CampaignController.java
@@ -3,10 +3,7 @@ package com.example.productcampaignbackend.controller;
 import com.example.productcampaignbackend.model.Campaign;
 import com.example.productcampaignbackend.repository.CampaignRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,11 +11,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1")
 public class CampaignController {
+
     @Autowired
     private CampaignRepository campaignRepository;
 
     @GetMapping("/campaignes")
     public List<Campaign> getAllCampaignes() {
         return campaignRepository.findAll();
+    }
+
+    @PostMapping("/campaignes")
+    public Campaign createCampaign(@RequestBody Campaign campaign) {
+        return campaignRepository.save(campaign);
     }
 }
